48b42d4c417a9a8c8de0aa6bfb868f6f85997cef
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-06-01 09:30:57 +0200

    Use an array instead of an object

--- src/scrapers.js
+++ src/scrapers.js
@@ -4,14 +4,14 @@ const Promise = require('promise');
 function slagthuset() {
     return new Promise((resolve, reject) => {
         let url = 'http://www.slagthus.se/';
-        let menu = {};
+        let menu = [];
 
         scrape(url, '.printable', week => {
-            menu.mon = week[0].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.tue = week[1].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.wed = week[2].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.thu = week[3].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.fri = week[4].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[0] = week[0].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[1] = week[1].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[2] = week[2].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[3] = week[3].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[4] = week[4].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').trim();
 
             resolve(menu);
         });
@@ -21,14 +21,14 @@ function slagthuset() {
 function meck() {
     return new Promise((resolve, reject) => {
         let url = 'http://meckok.se/lunch/';
-        let menu = {};
+        let menu = [];
 
         scrape(url, '#veckanslunch article', week => {
-            menu.mon = week[0].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.tue = week[1].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.wed = week[2].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.thu = week[3].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
-            menu.fri = week[4].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[0] = week[0].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[1] = week[1].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[2] = week[2].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[3] = week[3].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
+            menu[4] = week[4].slice(2).join(' ').replace('Dagens: ', '').replace(/\n+/g, '').replace(/\t+/g, '').trim();
 
             resolve(menu);
         });
@@ -38,17 +38,17 @@ function meck() {
 function miamarias() {
     return new Promise((resolve, reject) => {
         let url = 'http://www.miamarias.nu/';
-        let menu = {};
+        let menu = [];
 
         scrape(url, '.et-tabs-content', week => {
-            menu.mon = week[0].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
-            menu.tue = week[1].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
-            menu.wed = week[2].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
-            menu.thu = week[3].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
-            menu.fri = week[4].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
+            menu[0] = week[0].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
+            menu[1] = week[1].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
+            menu[2] = week[2].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
+            menu[3] = week[3].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
+            menu[4] = week[4].slice(1).join(' ').replace(/\n+/g, '').replace(/\t+/g, '').replace(/\d+/g, '').replace('/', '').replace(/Fisk|Kött|Vegetarisk/g, '').replace(/:-/g, ' ').trim();
 
-            menu.fri = menu.fri.split(' ');
-            menu.fri = menu.fri.slice(0, menu.fri.indexOf('Prenumera')).join(' ');
+            menu[4] = menu[4].split(' ');
+            menu[4] = menu[4].slice(0, menu[4].indexOf('Prenumera')).join(' ');
 
             resolve(menu);
         });
