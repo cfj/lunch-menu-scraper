9adb0086908b800212e0f308a3852a910b7064bb
Jonathan Svärdén <jonathan.svarden@gmail.com>
2016-03-16 19:06:09 +0100

    Remove debug code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import handlers from './handlers';
-import fs from 'fs';
 
 const app = express();
 
@@ -10,15 +9,5 @@ app.use(cors());
 app.get('/', handlers.rootHandler);
 
 app.get('/api/menus', handlers.apiHandler);
-
-app.get('/api/menus/local', (req, res) => {
-    fs.readFile('menus.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-
-        res.json(JSON.parse(data));
-    });
-});
 
 exports = module.exports = app;
